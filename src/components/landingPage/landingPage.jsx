8043921318d62a208497fb72b80c0beb066aa37d
bentleybuilds <bentleybuilds@gmail.com>
2020-01-22 10:16:04 -0700

    Initiating project, statically rendering components, added landingPage.scss

---
+++ src/components/landingPage/landingPage.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import './landingPage.scss'
+
+export const LandingPage = (props) => (
+    <div className='landingPage'>
+        <div className='directoryMenu'>
+            <div className = 'menuItem'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+        <div className='directoryMenu'>
+            <div className = 'menuItem'>
+                <div className='content'>
+                    <h1 className='title'>JACKETS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+        <div className='directoryMenu'>
+            <div className = 'menuItem'>
+                <div className='content'>
+                    <h1 className='title'>SNEAKERS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+        <div className='directoryMenu'>
+            <div className = 'menuItem'>
+                <div className='content'>
+                    <h1 className='title'>MENS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+        <div className='directoryMenu'>
+            <div className = 'menuItem'>
+                <div className='content'>
+                    <h1 className='title'>WOMENS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default LandingPage; 
