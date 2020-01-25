74f858333615a5d67c3237c3ba289893d75e74ca
bentleybuilds <bentleybuilds@gmail.com>
2020-01-25 13:07:18 -0700

    added withRouter to menuItems

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import LandingPage from './pages/landingPage/landingPage.jsx'
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <LandingPage />
+      <Switch>
+        <Route  exact path='/' component={LandingPage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
     </div>
     );
 }
--- src/components/menuItem/menuItem.jsx
+++ src/components/menuItem/menuItem.jsx
@@ -1,8 +1,9 @@
 import React from  'react'
+import { withRouter } from 'react-router-dom';
 import './menuItem.scss'
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className = {`${size} menuItem`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+    <div className = {`${size} menuItem`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div 
             className='backgroundImage'
             style={{
@@ -15,4 +16,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
         </div>
     </div>
 )
-export default MenuItem;
+export default withRouter(MenuItem);
