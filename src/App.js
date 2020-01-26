8992ddc3a2fbf40fd32fb52b8b9a0591352395b6
bentleybuilds <bentleybuilds@gmail.com>
2020-01-25 17:49:34 -0700

    added Header and Logo that links to home

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import LandingPage from './pages/landingPage/landingPage.jsx'
 import ShopPage from './pages/shop/shop.jsx'
+import Header from './components/header/header.jsx'
 
 
 
 function App() {
   return (
     <div>
+    <Header />
       <Switch>
         <Route  exact path='/' component={LandingPage} />
         <Route exact path='/shop' component={ShopPage} />
