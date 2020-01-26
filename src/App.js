b7ec4fa7edcbfb0893712d12e7b607a1a8249a1c
bentleybuilds <bentleybuilds@gmail.com>
2020-01-25 19:18:41 -0700

    created formInput and customButton components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import LandingPage from './pages/landingPage/landingPage.jsx'
 import ShopPage from './pages/shop/shop.jsx'
+import SignInAndSignUpPage from './pages/signIn/signInAndSignUp.jsx'
 import Header from './components/header/header.jsx'
 
 
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route  exact path='/' component={LandingPage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signIn' component={SignInAndSignUpPage} />
       </Switch>
     </div>
     );
---
+++ src/pages/signIn/signInAndSignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import SignIn from '../../components/signIn/signIn.jsx'
+
+import './signInAndSignUp.scss';
+
+const SignInAndSignUpPage = () => (
+    <SignIn className='signInAndSignUp' />
+);
+
+export default SignInAndSignUpPage;
