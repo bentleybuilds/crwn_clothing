110cf021ef466e79837128e9393850495dbd45bf
bentleybuilds <bentleybuilds@gmail.com>
2020-03-08 22:55:49 -0600

    pathing for images and init redux

--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -10,33 +10,33 @@ class Directory extends React.Component {
             sections: [
               {
                   title: 'hats',
-                  imageUrl: './images/hats.png', 
+                  imageUrl: 'images/hats.png', 
                   id: 1,
                   linkUrl: 'shop/hats'
           
               },
               {
                   title: 'jackets',
-                  imageUrl: './images/jackets.png', 
+                  imageUrl: 'images/jackets.png', 
                   id: 2,
                   linkUrl: 'shop/jackets'
               },
               {
                   title: 'sneakers',
-                  imageUrl: './images/sneakers.png', 
+                  imageUrl: 'images/sneakers.png', 
                   id: 3,
                   linkUrl: 'shop/sneakers'
               },
               {
                   title: 'womens',
-                  imageUrl: './images/womens.png', 
+                  imageUrl: 'images/womens.png', 
                   size: 'large',
                   id: 4,
                   linkUrl: 'shop/womens'
               },
               {
                   title: 'mens',
-                  imageUrl: './images/men.png', 
+                  imageUrl: 'images/men.png', 
                   size: 'large',
                   id: 5,
                   linkUrl: 'shop/mens'
