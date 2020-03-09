66e3641293b8b8d26a51d088fac60556054dcc09
bentleybuilds <bentleybuilds@gmail.com>
2020-03-08 22:14:31 -0600

    Updates

---
+++ precache-manifest.7badd8a4dac805e8b3cbe90ef795a6bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d986edd0a7a3e9892a1515ca904ad23",
+    "url": "/crwn_clothing/index.html"
+  },
+  {
+    "revision": "da8d8ef7f8e55679599f",
+    "url": "/crwn_clothing/static/css/main.9af52338.chunk.css"
+  },
+  {
+    "revision": "9bb96be3a2abc49cdaf1",
+    "url": "/crwn_clothing/static/js/2.9d8420e8.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/crwn_clothing/static/js/2.9d8420e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da8d8ef7f8e55679599f",
+    "url": "/crwn_clothing/static/js/main.66341eef.chunk.js"
+  },
+  {
+    "revision": "ea6c8bdaf30e5982e981",
+    "url": "/crwn_clothing/static/js/runtime-main.830b7fcd.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn_clothing/static/media/crown.ed28accb.svg"
+  }
+]);
