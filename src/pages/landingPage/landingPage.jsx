19fb3ac90d61d4390f84030b6cf072f973d4adc1
bentleybuilds <bentleybuilds@gmail.com>
2020-03-08 22:21:21 -0600

    set up build for github pages

--- src/pages/landingPage/landingPage.jsx
+++ src/pages/landingPage/landingPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './landingPage.scss'
 import Directory from '../../components/directory/directory.jsx'
 
-export const LandingPage = (props) => (
+const LandingPage = (props) => (
     <div className='landingPage'>
        <Directory />
     </div>
